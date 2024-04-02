ee6ed995446aa16550b0dfc15c03da3b7036e6af
Célio Júnior <celio_cjr@hotmail.com>
2024-04-02 18:57:24 +0930

    feat: creating the Header component with input. Installed lucide-react lib.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { AttendeeList } from "./components/AttendeeList"
+import { Header } from "./components/Header"
+
 function App() {
   return (
-    <div>hello</div>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+      <AttendeeList />
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+
+export function Header() {
+  return (
+    <div className='flex items-center gap-5 py-2'>
+      <img src={nlwUniteIcon} alt="Nlw Logo" />
+
+      <nav className='flex items-center gap-5'>
+        <a href="" className='font-medium text-sm text-zinc-300'>Events</a>
+        <a href="" className='font-medium text-sm'>Attendant</a>
+      </nav>
+
+    </div>
+  )
+}
