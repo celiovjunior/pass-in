16abc11abe95a069d4ee5be1b452e5892831b981
Célio Júnior <celio_cjr@hotmail.com>
2024-04-03 21:21:47 +0930

    feat: creating NavLink components, using ComponentProps

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+import { NavLink } from './NavLink';
 
 export function Header() {
   return (
@@ -6,8 +7,8 @@ export function Header() {
       <img src={nlwUniteIcon} alt="Nlw Logo" />
 
       <nav className='flex items-center gap-5'>
-        <a href="" className='font-medium text-sm text-zinc-300'>Events</a>
-        <a href="" className='font-medium text-sm'>Attendant</a>
+        <NavLink  href='/events'>Events</NavLink>
+        <NavLink href='/attendants'>Attendants</NavLink>
       </nav>
 
     </div>
