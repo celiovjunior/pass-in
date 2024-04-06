3d8fee9775657cb86eb0f0ebaa32050d51d238fe
Célio Júnior <celio_cjr@hotmail.com>
2024-04-06 17:05:44 +0930

    feat: setting up function to pass right pagination in the url

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -6,7 +6,7 @@ import { Table } from "./table/Table"
 import { TableHeader } from "./table/TableHeader"
 import { TableCell } from "./table/TableCell"
 import { TableRow } from "./table/TableRow"
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 
 dayjs.extend(relativeTime)
 
@@ -18,41 +18,65 @@ interface Attendee {
   checkedInAt: string | null
 }
 
+// URL State
+
 export function AttendeeList() {
-  // const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  });
   const [total, setTotal] = useState(0);
   const [attendees, setAttendees] = useState<Attendee[]>([]);
 
   const totalPages = Math.ceil(total / 10)
 
   useEffect(() => {
-    fetch(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page - 1}`)
+    const url = new URL("http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees")
+    url.searchParams.set("pageIndex", String(page - 1))
+
+    if (search.length > 0) {
+      url.searchParams.set("query", search)
+    }
+
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         setAttendees(data.attendees);
         setTotal(data.total);
       })
-  }, [page])
+  }, [page, search])
 
-  // function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
-  //   setSearch(event.target.value)
-  // }
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+    url.searchParams.set('page', String(page))
+    window.history.pushState({}, "", url)
+    setPage(page)
+  }
+
+  function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+    setCurrentPage(1)
+  }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   return(
@@ -63,7 +87,7 @@ export function AttendeeList() {
         </h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
-          <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Search..." />
+          <input onChange={onSearchInputChange} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Search..." />
         </div>
       </div>
 
@@ -108,7 +132,7 @@ export function AttendeeList() {
         <tfoot>
           <tr>
             <TableCell colSpan={3}>
-              Show 10 of {total} items
+              Show {attendees.length} of {total} items
             </TableCell>
             <TableCell className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
               {/* pagination */}
